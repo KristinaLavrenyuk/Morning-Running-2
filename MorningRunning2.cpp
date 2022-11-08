10ef0593105e462e846147e59b2041d65787a996
Kristina Lavrenyuk <115408741+KristinaLavrenyuk@users.noreply.github.com>
2022-11-08 22:18:38 +0300

    Add files via upload

---
+++ MorningRunning2.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float x, y; 
+	int i = 1;
+
+	cin >> x >> y;
+	float sum = x;
+
+	while (sum < y)
+	{
+		x += x * 0.7;
+		sum += x;
+		i++;
+	}
+	cout << i;
+	system("pause");
+}
